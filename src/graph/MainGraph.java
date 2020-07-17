e7d3122a7e0086084369295a9e680de33b0029b2
PaoloRoca <lmarch@ya.ru>
2020-07-17 04:02:16 +0300

    Home work 7. Add printf.

--- src/graph/MainGraph.java
+++ src/graph/MainGraph.java
@@ -9,6 +9,7 @@ public class MainGraph {
         Vertex ver3 = new Vertex(3);
         Vertex ver4 = new Vertex(4);
         Vertex ver5 = new Vertex(5);
+        Vertex ver6 = new Vertex(6);
 
         graph.addLink(ver1, ver2);
         graph.addLink(ver1, ver5);
@@ -18,20 +19,19 @@ public class MainGraph {
 
 //        graph.bfs(ver1);
 //        System.out.println();
+
         graph.dfs(ver1);
         System.out.println();
-
-        int length = graph.findMinLength(ver1, ver4);
-        System.out.println(length);
-
-        System.out.println("Кратчайшее расстояние между двумя вершинами: ");
+//        int length = graph.findMinLength(ver1, ver4);
+        int length = graph.findMinLength(ver1, ver6);
+        System.out.println();
         if (length > 0) {
-            System.out.println(String.format(" равно = %s", length));
+            System.out.println("Кратчайшее расстояние между двумя вершинами: " + length);
         } else if (length == 0) {
-            System.out.println(" A == B");
+            System.out.printf(" %s == %s ", ver1, ver4);
         } else {
-            System.out.println(String.format("A = %s %s", ver1, graph.isContain(ver1) ? "содержится в графе" : " не содержится в графе"));
-            System.out.println(String.format("B = %s %s", ver4, graph.isContain(ver4) ? "содержится в графе" : " не содержится в графе"));
+            System.out.printf("%s %s", ver1, graph.isContain(ver1) ? "содержится в графе" : " не содержится в графе");
+            System.out.printf("\n%s %s", ver4, graph.isContain(ver4) ? "содержится в графе" : " не содержится в графе");
         }
     }
 }
